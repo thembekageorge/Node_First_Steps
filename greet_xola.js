7dabbb269a4008e659a5047da1ce5e755bac6967
thembekageorge <thembiegeorge@gmail.com>
2017-06-05 16:20:36 +0200

    initial commit

---
+++ greet_xola.js
@@ -0,0 +1,22 @@
+//import the greet module that is in the current folder
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
+
+
+
+
+
+var figlet = require('figlet');
+
+figlet('Hello, Xola!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
